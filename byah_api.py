0a7964fb8c1aa67da48473d84eb080af68b8f2df
Cameron M <link_108@yahoo.com>
2016-03-10 08:38:03 -0500

    fixed routing

--- byah_api.py
+++ byah_api.py
@@ -1,29 +1,45 @@
-from bottle import route, run, template
+from bottle import route, run, template, default_app, Bottle
 
-@route('/api')
+app = application = Bottle()
+
+@app.route('/api')
 def hihi():
+    log("hit api")
     return '<b>hit api</b>!'
-    #return template('<b>hit api</b>!')
 
-@route('/api/')
+@app.route('/api/')
 def api_slash():
+    log("hit api/")
     return '<b>hit api/</b>!'
-    #return template('<b>hit api/</b>!')
 
-@route('/')
+@app.route('/')
 def slash():
+    log("hit /")
     return '<b>hit /</b>!'
-    #return template('<b>hit /</b>!')
 
-@route('*')
-def catch_all():
-    return '<b>hit /</b>!'
-    #return template('<b>hit /</b>!')
-
-@route('/api/*')
+@app.route('/api/hi')
 def catch_more():
-    return '<b>hit /api/*</b>!'
+    log("hit api/hi")
+    return '<b>hit /api/hi</b>!'
 
+@app.route('/api/byah')
+def catch_more():
+    log("hit api/byah")
+    return '<b>hit /api/byah</b>!'
+
+@app.route('/api/<url:re:.+>')
+def catch_more(url):
+    log("hit catch more")
+    return '<b>hit /api/%s</b>!' % url
+
+@app.route('/<url:re:.+>')
+def catch_all(url):
+    log("hit catch all")
+    return '<b>hit /%s</b>!' % url
+
+def log(message):
+  with open("byah_api.log", "a") as myfile:
+      myfile.write(message)
 
 if __name__ == "__main__":
-  run(host='0.0.0.0', port=5080)
+  app.run(host='0.0.0.0', port=5080)
